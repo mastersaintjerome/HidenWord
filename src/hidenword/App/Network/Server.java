0d1e65965a1c5c6afbf963b9a1bb285bea479192
moonlight83340 <gaetan.perrot@etu.univ-amu.fr>
2018-11-15 00:50:26 +0100

    start server

--- src/hidenword/App/Network/Server.java
+++ src/hidenword/App/Network/Server.java
@@ -1,5 +1,34 @@
 package hidenword.App.Network;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    final private ServerSocket ss;
+    private boolean running = true;
+    int id = 0;
+    
+    public Server(int port) throws IOException{
+        ss = new ServerSocket(port);
+    }
+    
+    public void run() throws IOException {
+        while (running){
+            Socket clientSocket = ss.accept();
+            ClientServerService cliThread = new ClientServerService(clientSocket, id++);
+        }
+    }
+    
+    public Socket accept () throws IOException {
+            return ss.accept();
+    }
+    
+    public void close() {
+        try {
+            running = false;
+            ss.close();
+        } catch (IOException e) {
+        }
+    }
 }
